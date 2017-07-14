ad7ac4b4ea6c43017ef2e6882b8c4c9ba8755702
Alex Aralis <alex.aralis@gmail.com>
2017-07-14 16:48:37 -0700

    add route animations

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import { RouteTransition } from 'react-router-transition';
 
 import HelloWorld from '~/components/HelloWorld';
 import Menubar from '~/components/Menubar';
@@ -15,16 +16,30 @@ import PrivateRoute from './PrivateRoute';
 export default () =>
   <div>
     <Menubar />
-    <Switch>
-      <Route exact path='/' component={HelloWorld} />
-      <Route path='/hello' component={HelloWorld} />
-      <Route path='/sign-up' component={SignUp} />
-      <Route path='/sign-in' component={SignIn} />
-      <Route path='/search' render={
-        () => <TutorSearchView maxSliderWage={50} minSliderWage={0} />
-      } />
-      <PrivateRoute path='/user' component={UserView} />
-      <Route component={NoMatch}/>
-    </Switch>
+    <Route render={({ location }) =>
+      <RouteTransition
+        pathname={location.pathname}
+        style={{ position: 'absolute', left: 0, right: 0 }}
+        atEnter={{ translateX: 100 }}
+        atLeave={{ translateX: -100 }}
+        atActive={{ translateX: 0 }}
+        mapStyles={styles => ({
+          position: 'absolute',
+          top: '0',
+          width: '100%',
+          transform: `translateX(${styles.translateX}%)`,
+        })} >
+        <Switch key={location.key} location={location}>
+          <Route exact path='/' component={HelloWorld} />
+          <Route path='/hello' component={HelloWorld} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/sign-in' component={SignIn} />
+          <Route path='/search' render={
+            () => <TutorSearchView maxSliderWage={50} minSliderWage={0} />
+          } />
+          <PrivateRoute path='/user' component={UserView} />
+          <Route component={NoMatch}/>
+        </Switch>
+      </RouteTransition>} />
   </div>
 ;
