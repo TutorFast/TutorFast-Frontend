4b363ccd127ea12ab99b1baacf9ba023e1c21889
Alex Aralis <alex.aralis@gmail.com>
2017-07-21 17:36:38 -0700

    add a loading bar to the index.html file

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const API_BASE = 'https://tutorfast-api.herokuapp.com';
+export const STRIPE_PUBLISHABLE_KEY = 'pk_test_Fycpa7AAvd8QzaRVLDcI7Gaw';
---
+++ src/stripe.js
@@ -0,0 +1,3 @@
+import STRIPE_PUBLISHABLE_KEY from '~/config';
+
+export default Stripe(STRIPE_PUBLISHABLE_KEY);
