c6292da9633fa92062b37a6d05d1e7a3a149620d
Alex Aralis <alex.aralis@gmail.com>
2017-07-23 14:15:31 -0700

    change back to deployed backend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// export const API_BASE = 'https://tutorfast-api.herokuapp.com';
-export const API_BASE = 'http://localhost:8888';
+export const API_BASE = 'https://tutorfast-api.herokuapp.com';
+// export const API_BASE = 'http://localhost:8888';
 
 export const STRIPE_PUBLISHABLE_KEY = 'pk_test_Fycpa7AAvd8QzaRVLDcI7Gaw';
