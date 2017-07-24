0cf11661f413e1ce5ccacc2776fb2890a4c4e743
Alex Aralis <alex.aralis@gmail.com>
2017-07-24 11:10:46 -0700

    change api base back to deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const API_BASE = 'https://tutorfast-api.herokuapp.com';
-export const API_BASE = 'http://localhost:8888';
+export const API_BASE = 'https://tutorfast-api.herokuapp.com';
+// export const API_BASE = 'http://localhost:8888';
 
 export const STRIPE_PUBLISHABLE_KEY = 'pk_test_Fycpa7AAvd8QzaRVLDcI7Gaw';
 export const STRIPE_CLIENT_ID = 'ca_B4BxMaYGwFZbmfB1TREDtMLazz0LkReC';
