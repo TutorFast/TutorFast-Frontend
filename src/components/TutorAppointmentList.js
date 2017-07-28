1afac82eafb1743085236a4e12e97a5efa8b55d6
Alex Aralis <alex.aralis@gmail.com>
2017-07-28 09:17:45 -0700

    complete display appointment list

--- src/components/TutorAppointmentList.js
+++ src/components/TutorAppointmentList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { List, Segment, Label } from 'semantic-ui-react';
+import { List, Segment, Label, Icon } from 'semantic-ui-react';
 
 import type User from '~/types/User';
 import { getAppointments } from '~/fetches';
@@ -26,11 +26,19 @@ class TutorAppointmentList extends Component {
   }
 
   readify = (start, end) =>
-    `On ${
-      new Date(start).toLocaleDateString()
-    } from ${
-      new Date(start).toLocaleTimeString()
-    } to ${new Date(end).toLocaleTimeString()}`
+    <div style={{ display: 'inline-block' }}>
+      <Label color='violet'>
+        <Icon name='calendar' />
+        {new Date(start).toLocaleDateString()}
+      </Label> from <Label color='blue' image>
+        {new Date(start).toLocaleTimeString()}
+        <Label.Detail>Start</Label.Detail>
+      </Label> <Icon name='angle right' />
+      <Label color='blue' image>
+        {new Date(end).toLocaleTimeString()}
+        <Label.Detail>End</Label.Detail>
+      </Label>
+    </div>
 
   stateToColor = state =>
     state === 'approved'
@@ -41,19 +49,50 @@ class TutorAppointmentList extends Component {
           ? 'orange'
           : 'red'
 
-  renderItem = ({ startDate, endDate, location, state }) =>
-    <List.Item>
-      <List.Header>{location}</List.Header>
-      <Label color={this.stateToColor(state)}>{state}</Label>
-      <List.Content>
-        {`${this.readify(startDate, endDate)}`}
+
+  stateToIcon = state =>
+    state === 'approved'
+      ? 'check'
+      : state === 'proposed'
+        ? 'question'
+        : state === 'rejected'
+          ? 'cancel'
+          : 'red'
+
+  renderItem = ({ startDate, endDate, location, state, cost, learner, tutor, subject }, idx) =>
+    <List.Item key={idx}>
+      <List.Content style={{ textAlign: 'center', marginBottom: '4px' }}>
+        <Label color={this.stateToColor(state)}>
+          <Icon name={this.stateToIcon(state)}/>
+          {state}
+        </Label>
+        <Label>
+          <Icon name='book'/>
+          {tutor.username}
+        </Label> teaches <Label color='pink'>
+          <Icon name='idea'/>
+          {subject}
+        </Label> to <Label>
+          <Icon name='student'/>
+          {learner.username}
+        </Label> for <Label tag color='olive'>
+          {`$${cost.toFixed(2)}`}
+        </Label>
+      </List.Content>
+      <List.Content style={{ textAlign: 'center' }}>
+        At <Label color='brown'>
+          <Icon name='marker'/>
+          {location}
+        </Label> on {
+          this.readify(startDate, endDate)
+        }
       </List.Content>
     </List.Item>
 
   render() {
     return (
       <Segment loading={this.state.loading} color={this.state.error ? 'orange' : null}>
-        <List divided relaxed>{do {
+        <List divided relaxed selection>{do {
           if (this.state.error)
             this.state.error;
           else if (this.state.appointments.length)
