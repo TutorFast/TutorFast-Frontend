61c431549065ebf2dd626d35198e5d4b944ed534
Alex Aralis <alex.aralis@gmail.com>
2017-07-29 10:54:42 -0700

    remove test from actions and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,3 @@
 export { default as signIn } from './signIn';
 export { default as signOut } from './signOut';
-export { default as test } from './test';
 export { default as updateUser } from './updateUser';
--- src/actions/test.js
+++
@@ -1,3 +0,0 @@
-import { TYPE } from '~/reducers/test';
-
-export default (str: string) => ({ type: TYPE, str });
