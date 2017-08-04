a6c8eb684f7be180d28640d1cf884d81663bcfe5
Alex Aralis <alex.aralis@gmail.com>
2017-08-03 18:16:22 -0700

    add the calendar

--- src/components/CreateAppointmentView.js
+++ src/components/CreateAppointmentView.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Segment } from 'semantic-ui-react';
+import Calendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
 
 import { getUser } from '~/fetches';
 import type User from '~/types/User';
 
 
+Calendar.momentLocalizer(moment);
+
 class CreateAppointmentView extends Component {
   state = {
     loading: null,
     tutor: null,
+    events: [],
   }
 
   componentWillMount() {
     this.loadTutor();
   }
 
-  componentWillRecieveProps() {
+  componentWillReceiveProps() {
     this.loadTutor();
   }
 
@@ -34,9 +41,20 @@ class CreateAppointmentView extends Component {
       }
     )
 
+  handleSelecting = ({ start, end }) => start >= Date.now()
+  handleSelectSlot = event => event.start >= Date.now() && this.setState({ events: [event] })
+
   render() {
+    console.log(this.state.events)
     return (
-      <Segment loading={this.state.loading}>
+      <Segment loading={Boolean(this.state.loading)}>
+        <Calendar selectable
+          defaultDate={new Date(Date.now())}
+          defaultView={'week'}
+          views={['week']}
+          events={this.state.events}
+          onSelecting={this.handleSelecting}
+          onSelectSlot={this.handleSelectSlot} />
         {this.props.tutorId}
         {this.state.tutor && JSON.stringify(this.state.tutor)}
         {this.props.learner && JSON.stringify(this.props.learner)}
